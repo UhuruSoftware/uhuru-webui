2855dc1223a7374d05b6aa79c3a8fb8e767efe69
ada.sescu <ada.sescu@uhurusoftware.com>
2012-10-05 16:49:04 +0300

    wrong class name
    Change-Id: I213841aeb66a73fddcd9d5a5d4d939f7d10ac577

--- lib/spaces.rb
+++ lib/spaces.rb
@@ -102,7 +102,7 @@ class Spaces
 
       app_uris = []
       app.routes.each do |r|
-        app_uris << Application::Url.new(r.host + "." + r.domain.name)
+        app_uris << Applications::Url.new(r.host + "." + r.domain.name)
       end
 
       apps_list << Applications::Application.new(app.name, app.runtime.name, app.guid, app.state, app_service_instances, app_uris, app.total_instances, app.memory)
