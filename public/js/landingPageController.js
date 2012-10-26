39507b0ff756357343890fe13bdf3a46adb3984a
Marius Ivan <marius.ivan@uhurusoftware.com>
2012-10-26 14:37:35 +0300

    sign up password verification script
    Change-Id: I8f62ebf29e727f6d1fa3d2c3bc3eabaebd0d8134

--- public/js/landingPageController.js
+++ public/js/landingPageController.js
@@ -12,13 +12,49 @@ var login_modal = function(){
 var signup_modal = function(){
 	$('#screen').css({	"display": "block", opacity: 0.7, "width": "10000px", "height": "10000px"});
 	$('body').css({"overflow":"hidden"});
+    $('#password').val('');
+    $('#confirm_password').val('');
+    $('#username').val('');
+    $('#email').val('');
 	$('#signup_modal').fadeIn(600);
-    $('.close').click(function(){$("#signup_modal").css("display", "none");$('#screen').css("display", "none");$('body').css("overflow", "auto");});
-    $('.btn').click(function(){$("#signup_modal").css("display", "none");$('#screen').css("display", "none");$('body').css("overflow", "auto");});
+    $('.close').click(function(){$("#signup_modal").css("display", "none");$('#screen').css("display", "none");$('body').css("overflow", "auto");$('#show_modal_errors').text('');});
+    $('.btn').click(function(){$("#signup_modal").css("display", "none");$('#screen').css("display", "none");$('body').css("overflow", "auto");$('#show_modal_errors').text('');});
 }
 
 
 $('#login_button').click(login_modal);
 $('#signup_button').click(signup_modal);
 
+});
+
+
+  jQuery(function(){
+        $(".sign_up_footer_modal").hover(function(){
+        $("#submit").hide();
+        var hasError = false;
+        var passwordVal = $("#password").val();
+        var checkVal = $("#confirm_password").val();
+        if (passwordVal == '') {
+            $('#show_modal_errors').text("Please enter a password");
+            hasError = true;
+        } else if (checkVal == '') {
+            $('#show_modal_errors').text("Please re-type the password to avoid mistake");
+            hasError = true;
+        } else if (passwordVal != checkVal ) {
+            $('#show_modal_errors').text("Passwords do not match!");
+            hasError = true;
+        }
+        if(hasError == true)
+        {
+            return false;
+        }
+        else
+        {
+            $("#submit").show();
+            $('#show_modal_errors').text("");
+        }
+    },
+        function(){
+            //$("#submit").show();
+        });
 });
