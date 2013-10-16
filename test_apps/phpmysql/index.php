300ff2275a2fa8af1e4d985717b3f096855916e4
Calin Miclaus <calin.miclaus@uhurusoftware.com>
2013-10-16 16:13:14 +0300

    rubymysql and phpmysql apps
    Change-Id: Ia107ab843c2babadb2a3e5f6e04f17eabe493186

---
+++ test_apps/phpmysql/index.php
@@ -0,0 +1,27 @@
+<?php
+$decodedDbData = json_decode($_SERVER["VCAP_SERVICES"], true);
+$credentials = $decodedDbData['mysql-5.1'][0]["credentials"];
+
+$con = mysql_connect($credentials["hostname"], $credentials["user"], $credentials["password"]);
+if (!$con)
+{
+	header('HTTP/1.1 500 Internal Server Error');
+	print mysql_error();
+}
+else
+{
+	$succeeded = mysql_select_db($credentials["name"]); 
+
+	if ($succeeded)
+		print 'success';
+	else
+	{
+		header('HTTP/1.1 500 Internal Server Error');
+		print mysql_error();
+	}
+		
+	mysql_close($con);
+}		
+
+
+?>
