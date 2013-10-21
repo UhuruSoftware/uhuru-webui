b2cbf06d6757c2763e8bbd42d75cdba24647c287
adasescu <ada.sescu@uhurusoftware.com>
2013-10-21 18:03:42 +0300

    changed nodejs with mysql test app.
    Change-Id: I98baee233cc552e267126ceb659bfa40c5d8c121

--- test_apps/nodejswithmysql/app.js
+++ test_apps/nodejswithmysql/app.js
@@ -2,24 +2,36 @@ require("cf-autoconfig");
 var express = require("express");
 var app = express();
 
-app.get('/', function(req, res) {
+var check_connection = function(req, res){
+
     var mysql      = require('mysql');
     var connection = mysql.createConnection();
 
     connection.connect();
-  
+
     connection.query('SELECT NOW() as time_now;', function(err, rows, fields) {
-    if (err) throw err;
+        if (err)
+        {
+            res.writeHead(500, {"Content-Type": "text/plain"});
+            res.write(String(err));
+            res.end();
+        }
+        else
+        {
+            res.writeHead(200, {"Content-Type": "text/plain"});
+            res.write(String(rows[0].time_now));
+            res.write("OK");
+            res.end();
+        }
 
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(rows[0].time_now);
-    res.end('\n');
-
-    console.log('The solution is: ');
+        console.log('The solution is: ' );
     });
+
     connection.end();
-    
-    res.send('Hello from Cloud Foundry');
+}
+
+app.get('/', function(req, res) {
+    check_connection(req, res);
 });
 
 app.listen(process.env.VCAP_APP_PORT || 3000);
