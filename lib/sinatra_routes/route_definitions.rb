9b9085dbdbbf9ff2fc35c50ebe8aacb605b87662
adasescu <ada.sescu@uhurusoftware.com>
2013-10-30 18:08:43 +0200

    UH-1697 added recaptcha tab in administration on webui
    Change-Id: I267d171dfe270f6f1d64b00913e2c6259c7c53dd

--- lib/sinatra_routes/route_definitions.rb
+++ lib/sinatra_routes/route_definitions.rb
@@ -45,6 +45,7 @@ module Uhuru::Webui
 
     ADMINISTRATION              = "/admin"
     ADMINISTRATION_WEBUI        = "#{ADMINISTRATION}/webui"
+    ADMINISTRATION_RECAPTCHA    = "#{ADMINISTRATION}/recaptcha"
     ADMINISTRATION_CONTACT      = "#{ADMINISTRATION}/contact"
     ADMINISTRATION_BILLING      = "#{ADMINISTRATION}/billing"
     ADMINISTRATION_EMAIL        = "#{ADMINISTRATION}/email"
